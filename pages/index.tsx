8ad9f967da98a6fde5de3e570ef145f2c90a45cf
tranquoc113 <tranquoc631@gmail.com>
2023-05-05 23:59:34 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,31 @@ import { NextPageWithLayout } from "@/models";
 import { MainLayout } from "components/layout";
 import Typography from '@mui/material/Typography';
 import { useWallet } from "@meshsdk/react";
+import { Seo } from "@/component/common/seo";
 
 
 const Home: NextPageWithLayout = () => {
   const {connected} = useWallet();
 
   return (
-    <Typography variant="h5" gutterBottom>
+    <>
+    <Seo
+        data={{
+          title: 'Blockchain | Update',
+          description:
+            'DCOneCrypto cardano-connect-wallet-tools Private',
+          url: 'https://learn-nextjs-fawn.vercel.app/',
+          thumbnailUrl:
+            'https://cdn.getshifter.co/caa65008efb706a8bfc6f7e4045d6a018420c3df/uploads/2020/11/nextjs.png',
+        }}
+      />
+        <Typography variant="h5" gutterBottom>
       {
         !connected?'Kết nối với ví để sử dụng':'Có thể gửi nhiều ada, mint, burn, tương tác với sc'
       }
     </Typography>
+    </>
+  
 
   );
 }
